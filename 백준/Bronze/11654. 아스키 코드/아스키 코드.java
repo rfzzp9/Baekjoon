7140b6b79f2be65093eaf76780f13ad277d5f997
Chaewon Gwak <106026889+rfzzp9@users.noreply.github.com>
2023-05-02 23:29:19 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17552 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        char n = sc.next().charAt(0);
+        
+        System.out.println((int)n);
+    }
+}
